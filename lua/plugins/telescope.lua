fd588167885e9a189f243a07433a65ab0b3a897d
Grant Boehringer <g2boehringer@gmail.com>
2025-07-27 01:37:55 -0500

    Initial configuration
    Signed-off-by: Grant Boehringer <g2boehringer@gmail.com>

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,11 @@
+return {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+-- or                              , branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      --lazy
+	{
+	    "nvim-telescope/telescope-file-browser.nvim",
+	    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+	},
+
+}
