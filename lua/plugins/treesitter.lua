f612d1019223f143cbfd1891b17e6757686b242b
Grant Boehringer <g2boehringer@gmail.com>
2026-03-09 20:18:43 -0500

    Install kanagawa
    Signed-off-by: Grant Boehringer <g2boehringer@gmail.com>

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,11 @@
+    return {
+      'nvim-treesitter/nvim-treesitter',
+      build = ':TSUpdate', -- This ensures parsers are updated on plugin updates
+      config = function()
+        require('nvim-treesitter.configs').setup {
+            highlight = {
+                enable = true
+            },
+        }
+      end,
+    }
