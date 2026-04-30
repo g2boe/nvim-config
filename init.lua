80628a23660121423fad5ca15c4c24857d93265a
Grant Boehringer <g2boehringer@gmail.com>
2026-04-30 03:56:48 -0500

    update formatting of init.lua

--- init.lua
+++ init.lua
@@ -4,24 +4,8 @@ require("options")
 -- You don't need to set any of these options.
 -- IMPORTANT!: this is only a showcase of how you can set default options!
 require("telescope").setup {
-  extensions = {
-    -- file_browser = {
-    --   theme = "ivy",
-    --   -- disables netrw and use telescope-file-browser in its place
-    --   hijack_netrw = true,
-    --   mappings = {
-    --     ["i"] = {
-    --       -- your custom insert mode mappings
-    --     },
-    --     ["n"] = {
-    --       -- your custom normal mode mappings
-    --     },
-    --   },
-    -- },
-  },
-  -- defaults = {
-  --     initial_mode = "normal"
-  -- }
+    extensions = {
+    },
 }
 -- To get telescope-file-browser loaded and working with telescope,
 -- you need to call load_extension, somewhere after setup function:
@@ -56,14 +40,14 @@ vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
 local cmp = require("cmp")
 
 cmp.setup({
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' } -- Source completions from the language server
-  }),
-  mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-u>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept selected item
-  }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' } -- Source completions from the language server
+    }),
+    mapping = cmp.mapping.preset.insert({
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-u>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept selected item
+    }),
 })
